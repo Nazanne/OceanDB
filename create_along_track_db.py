7b5f9864796f01d367c2687449f81a0f661d7478
Nazanne <bcurtis@mac.com>
2024-07-17 06:42:04 -0700

    Fix create database issues. Data not yet loading.

--- create_along_track_db.py
+++ create_along_track_db.py
@@ -1,9 +1,24 @@
 from AlongTrack import AlongTrackDatabase
+import os
 
-atdb = AlongTrackDatabase("localhost", "postgres")
+# Database access
+host = 'localhost'
+username = 'postgres'
+password = 'eJ^n$+%Ghwq3#oFW'
+port = 5432
+
+# Path to directory with files to be uploaded.
+directory = os.path.abspath(os.getcwd()) # Default to current active script directory
+
+atdb = AlongTrackDatabase(host, username, password, directory)
+# Build Database
 atdb.create_database()
 atdb.create_along_track_table()
 atdb.create_along_track_indices()
-# atdb.create_ocean_basin_connection_tables()
+atdb.create_ocean_basin_tables()
+atdb.create_ocean_basin_connection_tables()
+# Database build complete. Now load data
+atdb.insert_ocean_basins_from_csv()
+atdb.insert_ocean_basin_connections_from_csv()
 # atdb.create_along_track_table_partitions('monthly')
 # atdb.insert_along_track_data_from_netcdf('path/to/file.nc')
