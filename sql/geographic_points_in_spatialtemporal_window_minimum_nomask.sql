8373ba49239ea3912587a4a66b75af0fbbec5e3f
Jeffrey J. Early <JeffreyEarly@users.noreply.github.com>
2024-08-26 21:28:34 -0700

    Performance testing

---
+++ sql/geographic_points_in_spatialtemporal_window_minimum_nomask.sql
@@ -0,0 +1,8 @@
+SELECT
+	 longitude,
+     latitude,
+	 sla_filtered,
+	 EXTRACT(EPOCH FROM ({central_date_time} - date_time)) AS time_difference_secs
+FROM along_track alt
+WHERE ST_DWithin(ST_MakePoint(%(longitude)s, %(latitude)s),along_track_point, {distance})
+	AND date_time BETWEEN {central_date_time} - {time_delta} AND {central_date_time} + {time_delta};
