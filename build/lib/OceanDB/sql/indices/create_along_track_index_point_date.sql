6904298f90ef77fe374a38e1b6b4f1d08f26dfe7
Daniel Neshyba-Rowe <rowedanielp@gmail.com>
2026-01-08 16:45:00 -0800

    Remove build files

--- build/lib/OceanDB/sql/indices/create_along_track_index_point_date.sql
+++
@@ -1,3 +0,0 @@
-CREATE INDEX IF NOT EXISTS along_track_point_date_idx
-            ON along_track USING gist
-            (along_track_point, date_time);
--- build/lib/OceanDB/sql/indices/create_eddy_index_point.sql
+++
@@ -1,5 +0,0 @@
-CREATE INDEX IF NOT EXISTS eddy_point_idx
-            ON eddy USING gist
-            (eddy_point)
-            WITH (buffering=auto)
-            TABLESPACE pg_default;
