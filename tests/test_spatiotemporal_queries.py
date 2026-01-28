29304ea51175ed8f0e1ce61ccc9c38ebbda29eb1
Daniel Neshyba-Rowe <rowedanielp@gmail.com>
2026-01-28 11:31:26 -0800

    Fix geographic_points_in_r_dt test

--- tests/test_spatiotemporal_queries.py
+++ tests/test_spatiotemporal_queries.py
@@ -1,5 +1,6 @@
 import numpy as np
-from datetime import datetime
+import numpy.typing as npt
+from datetime import datetime, timedelta
 from OceanDB.data_access.along_track import AlongTrack
 
 def test_geographic_points_in_r_dt():
@@ -12,14 +13,20 @@ def test_geographic_points_in_r_dt():
     latitude = -69
     longitude = 28.1
     date = datetime(year=2019, month=1, day=1, hour=1)
+    radius = 500_000
+    time_window = timedelta(days=10)
 
     fields = list(AlongTrack.schema.keys())
     along_track_query_result_iterator = along_track.geographic_points_in_r_dt(
-        latitudes=np.array([latitude]), longitudes=np.array([longitude]), dates=[date], fields=fields
+        latitudes=np.array([latitude]), longitudes=np.array([longitude]), dates=[date], fields=fields, radii=radius, time_window = time_window,
     )
 
     along_track_output_list = list(along_track_query_result_iterator)
     result = along_track_output_list[0]
+    assert result is not None
     for field in fields:
         assert field in result
-    assert result['filename'] == 'asdfasdf'
+        # assert result[field].dtype == result.schema[field].python_type
+    assert "dt_global_alg_phy_l3_1hz_20190102_20240205.nc" in result['file_name']
+    assert ((result["date_time"] - date) <= time_window).all()
+    assert (result['distance'] <= radius).all()
